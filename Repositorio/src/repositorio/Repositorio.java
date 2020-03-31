bad8cdcc62ee248a37355c1f9369cbf045516f79
Jan Carlo De León <62573884+jancarlo-deleon@users.noreply.github.com>
2020-03-30 19:08:56 -0600

    Update Repositorio.java
    Adición de datos personales.

--- Repositorio/src/repositorio/Repositorio.java
+++ Repositorio/src/repositorio/Repositorio.java
@@ -7,6 +7,12 @@ public class Repositorio {
          System.out.println("Diego Alejandro Samayoa Gómez");
         System.out.println("Cambote Zona 11, Huehutenango");
         System.out.println("3130-8640");
+        
+        
+        System.out.println("JAN CARLO DE LEÓN CIFUENTES");
+        System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
+        System.out.println("33361433");
+        
     }
     
 }
