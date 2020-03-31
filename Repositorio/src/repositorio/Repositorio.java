8ca5ce4ff19e50f330dfc3dc1eab7b0e4c451651
User <User@LAPTOP-TBL2JDIU>
2020-03-30 18:45:31 -0600

    Prueba de repositorio, Diego Samayoa.

---
+++ Repositorio/src/repositorio/Repositorio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+/**
+ *
+ * @author User
+ */
+public class Repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Diego Alejandro Samayoa Gómez");
+        System.out.println("Cambote Zona 11, Huehutenango");
+        System.out.println("3130-8640");
+    }
+    
+}
