4b66d450cf61b2d8a741e1e7cfadb85d50061a27
JuanLariasMendez <jlariasm@miumg.edu.gt>
2020-03-30 22:47:34 -0600

    Update and rename Repositorio/src/repositorio/Repositorio.java to  Repositorio/ src/ repositorio/Repositorio.java

--- Repositorio/ src/ repositorio/Repositorio.java
+++ Repositorio/ src/ repositorio/Repositorio.java
@@ -4,6 +4,11 @@ public class Repositorio {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Juan Amilcar Larias Méndez");
+       System.out.println("Sector 3, cambote zona 11, Huehuetenango");
+       System.out.println("5737-5464");
+        
+        
          System.out.println("Diego Alejandro Samayoa Gómez");
         System.out.println("Cambote Zona 11, Huehutenango");
         System.out.println("3130-8640");
