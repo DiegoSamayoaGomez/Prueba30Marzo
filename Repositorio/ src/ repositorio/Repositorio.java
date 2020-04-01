dc417b303c3990080996800ebb3906b9f5dcba62
Carlos Andrés Miranda Méndez <62572969+CarlosMM28@users.noreply.github.com>
2020-04-01 11:27:35 -0600

    Update Repositorio.java

--- Repositorio/ src/ repositorio/Repositorio.java
+++ Repositorio/ src/ repositorio/Repositorio.java
@@ -14,7 +14,9 @@ public class Repositorio {
         System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
         System.out.println("33361433");
         
-        
+        System.out.println("Carlos Andrés Miranda Méndez");
+        System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
+        System.out.println("58385781");
         
         System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
         System.out.println("Direccion: Barillas Zona 6");
